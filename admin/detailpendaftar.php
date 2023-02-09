a0e48140b3aa3673f0ed53afe7814ad2be448981
Rizki fadillah <rizki.fadillah.20@student.is.ittelkom-sby.ac.id>
2023-02-09 22:32:24 +0700

    final

--- admin/detailpendaftar.php
+++ admin/detailpendaftar.php
@@ -7,7 +7,7 @@
 
   <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800">Detail Pendaftar</h1>
-  
+
   <div class="row">
     <div class="col-md-6">
       <div class="card shadow mb-4">
@@ -18,7 +18,7 @@
           <div class="card-body">
             <div class="col-auto mt-3 text-center">
               <?php
-              if(isset($data_pendaftar['foto']) && $data_pendaftar['foto'] != "") {
+              if (isset($data_pendaftar['foto']) && $data_pendaftar['foto'] != "") {
                 $foto = '../uploads/' . $data_pendaftar['foto'];
               } else {
                 $foto = '../assets/img/avatar.jpg';
@@ -37,8 +37,8 @@
               </li>
               <li class="list-group-item">
                 <h6 class="mb-0" style="color: black;">Jenis Kelamin</h6>
-                <?php 
-                if($data_pendaftar['jenis_kelamin'] == 'L') {
+                <?php
+                if ($data_pendaftar['jenis_kelamin'] == 'L') {
                   $kelamin = "Laki-laki";
                 } else {
                   $kelamin = "Perempuan";
@@ -49,7 +49,7 @@
               </li>
               <li class="list-group-item">
                 <h6 class="mb-0" style="color: black;">Agama</h6>
-                <small class="text-muted">Islam</small>
+                <small class="text-muted"><?= $data_pendaftar['agama'] ?></small>
               </li>
               <li class="list-group-item">
                 <h6 class="mb-0" style="color: black;">Alamat</h6>
@@ -77,25 +77,25 @@
         <div class="card-body">
           <div class="card-body">
 
-            <?php 
-            if($data_nilai['status'] == 0) {
+            <?php
+            if ($data_nilai['status'] == 0) {
               echo '
               <div class="alert alert-warning">
                   Data pendaftar belum divalidasi
               </div>';
-            } else if($data_nilai['status'] == 1) {
+            } else if ($data_nilai['status'] == 1) {
               echo '
               <div class="alert alert-info">
                   Data pendaftar Dinyatakan <b>LOLOS</b>
               </div>';
-            } else if($data_nilai['status'] == 2) {
+            } else if ($data_nilai['status'] == 2) {
               echo '
               <div class="alert alert-danger">
                   Data pendaftar Dinyatakan <b>TIDAK LOLOS</b>
               </div>';
             }
             ?>
-            
+
             <ul class="list-group">
               <li class="list-group-item">
                 <h6 class="mb-0" style="color: black;">Nilai Ujian Nasional</h6>
@@ -116,35 +116,35 @@
             </ul>
 
             <button type="button" class="btn btn-primary mt-3 btn-block" data-toggle="modal" data-target="#modalvalidasi">
-                Validasi Data Pendaftar
+              Validasi Data Pendaftar
             </button>
 
             <!-- Modal -->
             <div class="modal fade" id="modalvalidasi" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <form action="<?= $base_url ?>/admin/detailpendaftar.php?id=<?= $id_pendaftar ?>" method="POST">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Penilaian data pendaftar</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                          <label for="nilai">Beri Penilaian</label>
-                          <select name="nilai" id="nilai" class="form-control" required>
-                            <option value="">--Pilih --</option>
-                            <option value="1">Lolos</option>
-                            <option value="2">Tidak Lolos</option>
-                          </select>
-                        </div>
-                        <div class="modal-footer">
-                            <button name="simpan" value="simpan_nilai" class="btn btn-primary">Simpan</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                        </div>
-                      </form>
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <form action="<?= $base_url ?>/admin/detailpendaftar.php?id=<?= $id_pendaftar ?>" method="POST">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Penilaian data pendaftar</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
                     </div>
+                    <div class="modal-body">
+                      <label for="nilai">Beri Penilaian</label>
+                      <select name="nilai" id="nilai" class="form-control" required>
+                        <option value="">--Pilih --</option>
+                        <option value="1">Lolos</option>
+                        <option value="2">Tidak Lolos</option>
+                      </select>
+                    </div>
+                    <div class="modal-footer">
+                      <button name="simpan" value="simpan_nilai" class="btn btn-primary">Simpan</button>
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    </div>
+                  </form>
                 </div>
+              </div>
             </div>
           </div>
         </div>
@@ -152,9 +152,9 @@
 
       <a href="pendaftaran.php" class="btn btn-danger">Kembali</a>
     </div>
-    
+
   </div>
-  
+
 </div>
 <!-- /.container-fluid -->
 
